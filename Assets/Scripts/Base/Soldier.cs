8e48a6158cccf3f5941420009f4752b671a86469
DESKTOP-OHD6DA9\USER <kjp29816265@gmail.com>
2021-08-24 11:47:58 +0900

    제네릭 싱글톤 & json을 이용한 저장

---
+++ Assets/Scripts/Base/Soldier.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+
+public class Soldier
+{
+    public string name;
+    public int amount;
+    public long price;
+    public long ePs;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    [SerializeField]
+    private User user = null;
+
+    private string SAVE_PATH = "";
+    private readonly string SAVE_FILENAME = "/SaveFile.txt";
+    private void Start()
+    {
+        SAVE_PATH = Application.dataPath + "/Save";
+        if(!Directory.Exists(SAVE_PATH))
+        {
+            Directory.CreateDirectory(SAVE_PATH);
+        }
+
+        LoadFromJson();
+    }
+
+    private void LoadFromJson()
+    {
+        string json = "";
+
+        if(File.Exists(SAVE_PATH + SAVE_FILENAME))
+        {
+            json = File.ReadAllText(SAVE_PATH + SAVE_FILENAME);
+            user = JsonUtility.FromJson<User>(json);
+        }
+        else
+        {
+            SaveToJson();
+            LoadFromJson();
+        }
+    }
+
+    private void SaveToJson()
+    {
+        SAVE_PATH = Application.dataPath + "/Save";
+        if (user == null) return;
+        string json = JsonUtility.ToJson(user, true);
+        File.WriteAllText(SAVE_PATH + SAVE_FILENAME, json, System.Text.Encoding.UTF8);
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        SaveToJson();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveToJson();
+    }
+}
